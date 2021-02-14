301e00fa802d2819006e71789bd0ad1e940d502f
Rudolf Gasparian <69584395+ZionInMatrix@users.noreply.github.com>
2021-02-14 20:34:47 +0100

    Update Main.java
    Hotovo !!! Run Main...

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-     /**
+
+   /**
      * 
      */
 
-    
-
+public class Main {
+    public static void main(String[] args) {
+        
+        List<Employee> employees = new ArrayList<>();
+        DataManager dataManager = new DataManager();
+        
+        Employee employee = new Employee("Pavel Moravec","Java Developer","Java Developer","0","774 442 324","334 432 324f","pavel.moravec@vITStep.cz","IT Technology","IT","IT");
+        employees.add(employee);
+        
+        dataManager.writeToCSV(employees);
+        dataManager.readDataFromCsvFile();
     }
 
 }
