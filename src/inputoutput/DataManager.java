62f9b64e228d068e3d320c20a49ca952a459eb9f
Rudolf Gasparian <69584395+ZionInMatrix@users.noreply.github.com>
2021-02-14 20:32:29 +0100

    Update DataManager.java
    Hotovo.

--- src/inputoutput/DataManager.java
+++ src/inputoutput/DataManager.java
@@ -1,52 +1,49 @@
 package inputoutput;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class DataManager {
     private static final String CSV_SEPARATOR = ";";
     private static final String fileName = System.getProperty("user.dir") + "/src/Tel.csv";
 
     /**
-     * Snazim se udelat metodu ktera bude nacitat Data from CSV file.
+     *
      */
-    public void readDataFromCsvFile() {
+    public List<Employee> readDataFromCsvFile() {
         String line = "";
         BufferedReader br;
         List<Employee> employees = new ArrayList<>();
-            try {
-                br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "Windows-1250"));
-                while ((line = br.readLine()) != null) {
-                    String[] values = line.split(";");
-                    Employee employee = new Employee();
-                    employee.setJmeno(values[0]);
-                    employee.setFunkce(values[1]);
-                    employee.setPopisCinnosti(values[2]);
-                    employee.setCisloDveri(values[3]);
-                    employee.setTelefon(values[4]);
-                    employee.setMobil(values[5]);
-                    employee.setEmail(values[6]);
-                    employee.setOddeleni(values[7]);
-                    employee.setZkratkaOdboru(values[8]);
-                    employee.setNazevOdboru(values[9]);
-                    employees.add(employee);
-                }
-                System.out.println(employees);
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "Windows-1250"));
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                Employee employee = new Employee();
+                employee.setJmeno(values[0]);
+                employee.setFunkce(values[1]);
+                employee.setPopisCinnosti(values[2]);
+                employee.setCisloDveri(values[3]);
+                employee.setTelefon(values[4]);
+                employee.setMobil(values[5]);
+                employee.setEmail(values[6]);
+                employee.setOddeleni(values[7]);
+                employee.setZkratkaOdboru(values[8]);
+                employee.setNazevOdboru(values[9]);
+                employees.add(employee);
             }
+            System.out.println(employees);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return employees;
+    }
 
     /**
      * Udelal jsem metodu, ktera bude nacitat data do CSVFilu
      */
 
-    public void writeToCSV(ArrayList<Employee> employees) {
+    public void writeToCSV(List<Employee> employees) {
         try {
             BufferedWriter bw = null;
             try {
-                bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "Windows-1250"));
+                bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName, true), "Windows-1250"));
             } catch (UnsupportedEncodingException | FileNotFoundException e) {
                 e.printStackTrace();
             }
@@ -81,4 +78,5 @@ public class DataManager {
         }
     }
 }
+
 
